72dd3b9ae6cf93d401bd952beadd9f815c8c67ca
Paulo Córdova <control@pcgoperacion.com>
2026-04-30 16:56:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7816109963-74959",
+  "appId": "1:182576941186:web:97ded44b708716ca9e0c9b",
+  "apiKey": "AIzaSyCENoI1a_fwd8EeHFV2KGfvvfrhb-b-Jyk",
+  "authDomain": "studio-7816109963-74959.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "182576941186"
+};
