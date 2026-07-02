3eadc9eee9e50cda2598447ce27d8e19d207864b
Paulo Córdova <control@pcgoperacion.com>
2026-07-02 15:45:40 +0000

    me sigue dando este error, git push remote: Permission to pcordova-star/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -586,7 +586,7 @@ export default function HomePage() {
                 <motion.div 
                   key={i}
                   initial={{ opacity: 0, y: 20 }}
-                  whileInView={{ opacity: 1, y: 0 }}
+                  whileInView={{ opacity: 1, x: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: i * 0.1 }}
                   className="relative group"
